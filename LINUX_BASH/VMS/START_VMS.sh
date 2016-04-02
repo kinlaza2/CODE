b371eac73042948745dc96f6a2f12b9c550e17db
ni2 <nis@test.com>
2016-04-02 14:13:53 +0300

    KANNEL DLR SCRIPT

---
+++ LINUX_BASH/VMS/START_VMS.sh
@@ -0,0 +1,2 @@
+/bin/rm -rf /home/lnikos/VM__MACHINES/RHE_5_2_NIK1_EXTERNAL_ACCESS/*.lck
+/usr/bin/vmrun  -T ws start /home/lnikos/VM__MACHINES/RHE_5_2_NIK1_EXTERNAL_ACCESS/Red\ Hat\ Enterprise\ Linux\ 5.vmx nogui
