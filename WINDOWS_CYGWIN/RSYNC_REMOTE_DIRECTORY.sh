adc286f3a65933612febb0ce7f30fe3ea9486623
kinzala2@gmail.com <kinzala2@gmail.com>
2017-06-26 18:58:29 +0300

    sd

--- WINDOWS_CYGWIN/RSYNC_REMOTE_DIRECTORY.sh
+++ WINDOWS_CYGWIN/RSYNC_REMOTE_DIRECTORY.sh
@@ -4,7 +4,12 @@ LOCAL_DIRECTORY=/cygdrive/d/CD/BACKUP_CD_INFRA/60/NAGIOS/
 REMOTE_SERVER=10.1.1.60
 REMOTE_DIRECTORY=/home/nagios/TEMP/
 REMOTE_USER=root
+SCRIPT_PATH=/home/ni_new/SYNC_SERVERS_JOB/
+LOG_FILE=NAGIOS_SYN.log
 
 
+echo -e "-------------------------- PROCESS START ON `date` -------------------------------------------------------------------------------------------" >> $SCRIPT_PATH/$LOG_FILE
+rsync -azPv --delete $REMOTE_USER@$REMOTE_SERVER:$REMOTE_DIRECTORY   $LOCAL_DIRECTORY >> $SCRIPT_PATH/$LOG_FILE
+echo -e "-------------------------- PREOCESS FINISHED ON `date` ---------------------------------------------------------------------------------------" >> $SCRIPT_PATH/$LOG_FILE
 
-rsync -azPv $REMOTE_USER@$REMOTE_SERVER:$REMOTE_DIRECTORY   $LOCAL_DIRECTORY
+
